e9a1dec2e71c55a9d0c2bbda9198f66658033d9d
gunyongbok <qhrrjsdyd123@gmail.com>
2023-09-10 19:34:41 +0900

    fix: styled component 오류 수정

--- src/components/Wrapper/TopContainer.tsx
+++ src/components/Wrapper/TopContainer.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { styled } from "styled-components";
 
 interface ContainerProps {
-  background?: string;
+  $background?: string;
   children: ReactNode;
 }
 
@@ -15,7 +15,7 @@ const Container = styled.div<ContainerProps>`
   box-sizing: border-box;
   position: relative;
   background: ${(props) =>
-    props.background ||
+    props.$background ||
     "linear-gradient(0deg, rgba(255, 255, 255, 0.40) 0%, rgba(255, 255, 255, 0.40) 100%), linear-gradient(168deg, #73DE9D 0%, #86CDA2 19.47%, #44D67D 44.22%, #86CDA2 99.11%)"};
   @media (max-width: 599px) {
     height: 100vh;
@@ -23,8 +23,8 @@ const Container = styled.div<ContainerProps>`
   }
 `;
 
-const TopContainer = ({ children, background }: ContainerProps) => {
-  return <Container background={background}>{children}</Container>;
+const TopContainer = ({ children, $background }: ContainerProps) => {
+  return <Container $background={$background}>{children}</Container>;
 };
 
 export default TopContainer;
--- src/page/Login/ServiceAgree.tsx
+++ src/page/Login/ServiceAgree.tsx
@@ -3,14 +3,16 @@ import { styled } from "styled-components";
 // Container
 import TopContainer from "../../components/Wrapper/TopContainer";
 
-import backArrowImg from "../../assets/img/back.png";
-
-import fisrtProgress from "../../assets/img/progress1.png";
+// Header
 import MainHeader from "../../components/Header/MainHeader";
+
+// Image
+import backArrowImg from "../../assets/img/back.png";
+import fisrtProgress from "../../assets/img/progress1.png";
 
 const ServiceAgree = () => {
   return (
-    <TopContainer background="#FCFCFF">
+    <TopContainer $background="#FCFCFF">
       <MainHeader src1={backArrowImg} src2={fisrtProgress} />
     </TopContainer>
   );
