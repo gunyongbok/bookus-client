5d2092a9f51945ece37b5b72cb67c3759872c721
gunyongbok <qhrrjsdyd123@gmail.com>
2023-09-22 14:02:00 +0900

    feat: 카카오 로그인 구현

--- src/page/Login/Kakao.tsx
+++ src/page/Login/Kakao.tsx
@@ -1,4 +1,29 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 const Kakao = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const code = searchParams.get("code");
+  console.log(code);
+
+  const ApiCall = async () => {
+    try {
+      const response = await axios.get(
+        `${
+          import.meta.env.VITE_APP_DEFAULT_SERVER_URL
+        }/oauth/kakao?code=${code}`
+      );
+      console.log("response >>", response);
+    } catch (err) {
+      console.log("Error >>", err);
+    }
+  };
+
+  useEffect(() => {
+    ApiCall();
+  }, []);
   return <div>KaKao</div>;
 };
 
