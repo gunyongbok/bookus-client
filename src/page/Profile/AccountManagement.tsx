ce11bd44f2c563482bb01bf6e73ee6b8bf5f3918
gunyongbok <qhrrjsdyd123@gmail.com>
2024-05-09 11:32:25 +0900

    error handling

--- src/page/Profile/AccountManagement.tsx
+++ src/page/Profile/AccountManagement.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 // Container
